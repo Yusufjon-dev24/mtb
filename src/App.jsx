3abc60a1a873ff62cdcd240e5f36417064f3cd20
Yusufjon-dev24 <y1690659@gmail.com>
2025-12-24 18:59:30 +0500

    headerni tugatdim

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTelegram } from "@fortawesome/free-brands-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import rasm1 from "./russ.png"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div>
-      salom
-    </div>
+      <div>
+        <div className="header-container">
+          <div className="logo">
+            <h1>MTB</h1>
+          </div>
+          <div className="case">
+            <div className="search"><FontAwesomeIcon icon={faMagnifyingGlass} /></div>
+            <div className="language"><img src={rasm1} width={25} height={25} style={{borderRadius:50}} alt=""/> <h3>Ru</h3></div>
+            <div className="subscribe">
+              <button>
+                <FontAwesomeIcon icon={faTelegram} /> Подписаться
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
